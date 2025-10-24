ab09777d54b99cff1e5dabf457b02d53d5492190
Brayan-Jeremy <gonzalezgbrayanj@gmail.com>
2025-10-23 20:10:54 -0600

    test

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,3 +5,9 @@ class StaticPagesController < ApplicationController
   def help
   end
 end
+describe "GET #about" do
+  it "returns http success" do
+    get :about
+    expect(response).to have_http_status(:success)
+  end
+end
